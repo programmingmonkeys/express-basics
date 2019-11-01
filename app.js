ab7cd6af5f6954c260a9f427571e999333a5191d
codingChewie <matthewtimothy910@yahoo.com>
2019-11-01 13:24:56 -0500

    Adding Multiple Routes to the App

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('<h1>Home page</h1>')
+})
+
+app.get('/', (req, res) => {
+  res.send('<h1>Home page</h1>')
+})
+
+app.listen(3000, () => console.log('The app is running on port 3000'))
