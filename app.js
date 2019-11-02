bae5e8eaa356f74c8300837b7b9f785611dd5dff
codingChewie <matthewtimothy910@yahoo.com>
2019-11-01 20:53:29 -0500

    Middleware Sequence and Routing

--- app.js
+++ app.js
@@ -9,6 +9,16 @@ app.use(cookieParser())
 
 app.set('view engine', 'pug')
 
+app.use((req, res, next) => {
+  req.message = 'message made it'
+  next()
+})
+
+app.use((req, res, next) => {
+  console.log(req.message)
+  next()
+})
+
 app.get('/', (req, res) => {
   const name = req.cookies.username
 
