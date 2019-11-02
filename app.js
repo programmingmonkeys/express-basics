2bb20266edf004effd3ec06b53a248fcf8ac9801
codingChewie <matthewtimothy910@yahoo.com>
2019-11-02 13:01:48 -0500

    Adding Static Assets to the App

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const app = express()
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 
+app.use('/static', express.static('public'))
 app.set('view engine', 'pug')
 
 const mainRoutes = require('./routes/index')
