1cde09daa11e0406f1f0ec1837240336f465490c
codingChewie <matthewtimothy910@yahoo.com>
2019-11-02 12:23:39 -0500

    Linking Around the Application

--- routes/cards.js
+++ routes/cards.js
@@ -17,7 +17,7 @@ router.get('/:id', (req, res) => {
   const { side } = req.query
   const { id } = req.params
 
-  if (!side) res.redirect(`/cards/${id}?side=question`)
+  if (!side) return res.redirect(`/cards/${id}?side=question`)
 
   const name = req.cookies.username
   const text = cards[id][side]
