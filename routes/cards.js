72fa8e90a4332f2bb04becd1f5ad4468151decd0
codingChewie <matthewtimothy910@yahoo.com>
2019-11-02 11:22:24 -0500

    Using Data and Route Parameters

--- routes/cards.js
+++ routes/cards.js
@@ -1,12 +1,23 @@
 const express = require('express')
 const router = express.Router()
 const eyes = require('eyespect')
+const { data } = require('../data/flashcardData.json')
+const { cards } = data
 
-router.get('/', (req, res) => {
-  eyes.inspect(res.locals, 'res.locals')
+router.get('/:id', (req, res) => {
+  res.render('card', {
+    prompt: cards[req.params.id].question,
+    hint: cards[req.params.id].hint,
+  })
+})
+
+// Sample testing req.params
+router.get('/:foo/:bar', (req, res) => {
+  eyes.inspect(req.params, 'req.params') // http://expressjs.com/en/api.html#req.params
 
   res.render('card', {
-    prompt: `Who is buried in Grant's tomb?`,
+    prompt: cards[req.params.foo].question,
+    hint: cards[req.params.bar].hint,
   })
 })
 
